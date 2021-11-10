4bf2e171b89157c7e8e3169a9b332dd0457fca37
Djénéba Samyra Nougnymeh SAKO <24464@student.dorset-college.ie>
2021-11-10 17:40:32 +0100

    Ajoutez des fichiers projet.

---
+++ Models/InstrumentLibray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntrumentLibrary.Models
+{
+    class InstrumentLibray
+    {
+        public abstract class Instrument
+        {
+            public string serialNumber { get; set; }
+            public string Name { get; set; }
+
+            public Instrument (string Name)
+            {
+                this.Name = Name;
+            }
+
+            public abstract void Play();
+        }
+
+        public class Guitar : Instrument
+        {
+            public Guitar(string Name) : base(Name)
+            {
+            }
+
+            public override void Play()
+            {
+                Console.WriteLine($"I am playing {Name} Guitar");
+            }
+        }
+        public class Mandolin : Instrument
+        {
+            public Mandolin(string Name) : base(Name)
+            {
+            }
+
+            public override void Play()
+            {
+                //throw new NotImplementedException();
+                Console.WriteLine($"I am playing {Name} Mandolin");
+            }
+        }
+
+        public class Ukele : Instrument
+        {
+            public Ukele(string Name) : base(Name)
+            {
+            }
+
+            public override void Play()
+            {
+                //throw new NotImplementedException();
+                Console.WriteLine($"I am playing {Name} Ukele");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IntrumentLibrary.Models.InstrumentLibray;
+
+namespace IntrumentLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guitar myGuitar = new Guitar("Gibson");
+            Mandolin myMandolin = new Mandolin("Kentuky");
+            Ukele myUkele = new Ukele("Ortega");
+
+            myUkele.serialNumber = "abc123";
+            myGuitar.Play();
+            myMandolin.Play();
+            myUkele.Play();
+
+            Console.ReadKey();
+        }
+
+    }
+}
